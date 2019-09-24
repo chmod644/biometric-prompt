38b5f0df67723e210c16a0c5af77ec42e23b82c2
chmod644 <chmod644@users.noreply.github.com>
2019-09-24 15:07:52 +0900

    Add 3 fragments

---
+++ app/src/main/java/com/example/sandbox/ui/main/FirstViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.sandbox.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class FirstViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/sandbox/ui/main/ThirdViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.sandbox.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class ThirdViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
